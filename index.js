0364b90dfe038d9bb8c4702e6a67f997fecccef5
cark0s <96215640+cark0s@users.noreply.github.com>
2024-06-07 16:02:29 -0400

    subida desde hvn

--- index.js
+++ index.js
@@ -1,5 +1,49 @@
-console.log('Conexión intento');
-const {conexion}= require('./database/conexion');
+const { conexion } = require('./database/conexion');
+const cors = require('cors');
+const express = require('express');
 
-//iniciar la conexion
+// Iniciar la conexión a la base de datos
 conexion();
+
+// Crear servidor en Node
+const app = express();
+const puerto = 3900;
+
+// Configurar CORS
+app.use(cors());
+
+// Convertir el cuerpo de las solicitudes en JSON
+app.use(express.json());
+
+// Crear ruta para la raíz
+app.get('/', (req, res) => {
+    return res.status(200).json([{
+        curso: "Curso de Node.js",
+        autor: "Jhonny",
+        url: "https://www.udemy.com/course/curso-de-node-js/",
+    },
+{
+    curso: "Curso de Node.js",
+    autor: "Jhonny",
+    url: "https://www.udemy.com/course/curso-de-node-js/",
+},
+
+]);
+
+});
+
+// Crear ruta /probando
+app.get('/probando', (req, res) => {
+    console.log('Hola mundo');
+    return res.status(200).send(`
+        <div>
+            <h1>Hola mundo</h1>
+        </div>
+    `);
+});
+
+// Crear servidor y escuchar peticiones HTTP
+app.listen(puerto, () => {
+    console.log('Servidor iniciado en http://localhost:' + puerto);
+});
+
