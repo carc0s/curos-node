92116e689ed0e72d4533bdf92edca4874296dc8b
cark0s <96215640+cark0s@users.noreply.github.com>
2024-06-13 14:43:52 -0400

    otro curso

---
+++ api-res-redsocial/index.js
@@ -0,0 +1,50 @@
+const { conexion } = require('./datebase/conexion');
+const cors = require('cors');
+const express = require('express');
+
+// Iniciar la conexión a la base de datos
+conexion();
+// Crear servidor en Node
+const app = express();
+const puerto = 3900;
+
+// Configurar CORS
+app.use(cors());
+
+// Convertir el cuerpo de las solicitudes en JSON
+app.use(express.json());//recibir datos con content-type app/json
+
+app.use(express.urlencoded({ extended: true }));//recibir datos con content-type app/x-www-form-urlencoded
+
+
+// Crear ruta para la raíz
+app.get('/', (req, res) => {
+    return res.status(200).json([{
+        curso: "Curso de Node.js",
+        autor: "Jhonny",
+        url: "https://www.udemy.com/course/curso-de-node-js/",
+    },
+{
+    curso: "Curso de Node.js",
+    autor: "Jhonny",
+    url: "https://www.udemy.com/course/curso-de-node-js/",
+},
+
+]);
+
+});
+
+// Crear ruta /probando
+app.get('/probando', (req, res) => {
+    console.log('Hola mundo');
+    return res.status(200).send(`
+        <div>
+            <h1>Hola mundo</h1>
+        </div>
+    `);
+});
+
+// Crear servidor y escuchar peticiones HTTP
+app.listen(puerto, () => {
+    console.log('Servidor iniciado en http://localhost:' + puerto);
+});
