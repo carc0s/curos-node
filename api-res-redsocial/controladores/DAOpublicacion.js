046354826d12ac823cee70f63b1c0fedda0703c9
cark0s <96215640+cark0s@users.noreply.github.com>
2024-06-28 10:11:43 -0400

    cambios

--- api-res-redsocial/controladores/DAOpublicacion.js
+++ api-res-redsocial/controladores/DAOpublicacion.js
@@ -1,28 +1,33 @@
 //acciones de prueba
 const publicacion = require('../modelos/publicacion');
+const mongoose = require('mongoose-pagination');
 
 
 //guardar publicacion
-const savePublicacion =async(req,res)=>{
+const savePublicacion = async (req, res) => {
     const params = req.body;
-    const sesion = req.user;
 
-    //validar datos
-    
+
+    // Validar datos
+    if (!params.texto) {
+        return res.status(400).json({
+            status: "error",
+            message: "Faltan datos por enviar"
+        });
+    }
+
     // Crear objeto a guardar
-    let publicacionGuardar = new Seguir({
-        id_usuario: sesion._id,
-        id_seguidor: params.seguir,
-    });
+    let publicacionGuardar = new publicacion(params);
+    publicacionGuardar.user = req.user._id;
 
     try {
         // Guardar en la base de datos 
-        await nuevoSeguimiento.save();
+        await publicacionGuardar.save();
 
         return res.status(200).json({
             status: "success",
-            message: "Seguir a un usuario",
-            seguimiento: nuevoSeguimiento,
+            message: "Publicación guardada correctamente",
+            publicacion: publicacionGuardar,
         });
     } catch (error) {
         console.error(error);
@@ -32,31 +37,245 @@ const savePublicacion =async(req,res)=>{
             error: error.message,
         });
     }
-
-
-
-
-
-
-
-
-
-
-
-    return res.status(200).json({
-        status: "success",
-        message: "Publicacion registrado exitosamente",
-     
-      });
-
-
 }
-//listar todas las publicaciones
-
 
 //listar publicacion de un usuario
+const buscarPublicacion = async (req, res) => {
+    try {
+        // Obtener usuario autenticado
+        const id = req.params._id;
+        
+        // Validar que se ha proporcionado un ID
+        if (!id) {
+            return res.status(400).json({
+                status: "error",
+                message: "ID de usuario es requerido",
+            });
+        }
+
+        // Buscar usuario en la base de datos
+        const publi = await publicacion.findById(id);
+
+        // Verificar si el usuario ya existe
+        if (!publi) {
+            return res.status(404).json({
+                status: "error",
+                message: "publicacion no encontrado",
+            });
+        }
+
+        return res.status(200).json({
+            status: "success",
+            message: "publicacion encontrada",
+            publi,
+
+        });
+    } catch (error) {
+        console.error(error); // Log del error para depuración
+        return res.status(500).json({
+            status: "error",
+            message: "Error del servidor",
+            error: error.message,
+        });
+    }
+}
 
 //eliminar publicaciones
+
+const EliminarPublicacion = async (req, res) => {
+    const publiId = req.params.id;
+
+
+    try {
+        // Validar que el ID de seguimiento existe
+        if (!publiId) {
+            return res.status(400).json({
+                status: "error",
+                message: "El ID del seguimiento es necesario."
+            });
+        }
+
+        // Eliminar el seguimiento de la base de datos
+        const result = await publicacion.findOneAndDelete({
+            _id: publiId,
+            user: req.user._id
+        });
+
+        if (!result) {
+            return res.status(404).json({
+                status: "error",
+                message: "No se encontró la publicacion.",
+            });
+        }
+
+        return res.status(200).json({
+            status: "success",
+            message: "Publicacion eliminada con éxito",
+            idEliminado: publiId
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            status: "error",
+            message: "Error al eliminar la información",
+            error: error.message,
+        });
+    }
+};
+
+const obtenerPaginacion = (req) => {
+    const page = req.params.page ? parseInt(req.params.page, 10) : 1;
+    const perPage = 2;
+    return { page, perPage };
+};
+
+
+// Listar las publicaciones de un usuario
+const listarPublicacionesUsuario = async (req, res) => {
+    try {
+        // Obtener el ID del usuario
+        const userId = req.params.id;
+        const { page, perPage } = obtenerPaginacion(req);
+
+        // Validar que se ha proporcionado un ID de usuario válido
+        if (!userId ) {
+            return res.status(400).json({
+                status: "error",
+                message: "ID de usuario es requerido y debe ser válido",
+            });
+        }
+
+        // Buscar las publicaciones del usuario en la base de datos
+        const publicaciones = await publicacion.find({ user: userId }).populate('user','-bio -password -rol -image -fecha -__v')
+            .sort( "-fecha"  ) // Ordenar por ID descendente
+            .skip((page - 1) * perPage) // Saltar los documentos según la página
+            .limit(perPage); // Limitar la cantidad de documentos a mostrar
+
+            if(!publicaciones || publicaciones.length === 0){
+                return res.status(404).json({
+                    status: "error",
+                    message: "No se encontraron publicaciones del usuario",
+                });
+            }
+        // Obtener el total de publicaciones
+        const total = await publicacion.countDocuments({ user: userId });
+
+        return res.status(200).json({
+            status: "success",
+            message: "Publicaciones del usuario encontradas",
+            publicaciones,
+            totalPublicaciones: total,
+            totalPages: Math.ceil(total / perPage),
+            currentPage: page,
+        });
+    } catch (error) {
+        console.error(error); // Log del error para depuración
+        return res.status(500).json({
+            status: "error",
+            message: "Error del servidor",
+            error: error.message,
+        });
+    }
+};
+
+
+//subir imagen
+const subirImagen = async (req, res) => {
+    // Recoger el fichero de la imagen
+    if (!req.file || req.file == undefined) {
+      return res.status(400).json({
+        mensaje: "sin imagen"
+      });
+    }
+  
+    // Nombre del archivo
+    let nombrearchivo = req.file.originalname;
+    const idpublicacion = req.params.id;
+    // Extensión del archivo
+    let extencion = nombrearchivo.split("\.");
+    let archivoextencion = extencion[1].toLowerCase(); // Convertir la extensión a minúsculas para evitar problemas de validación
+  
+    // Comprobar extensión del archivo
+    if (archivoextencion != "png" && archivoextencion != "jpg" && archivoextencion != "jpeg" && archivoextencion != "gif") {
+      // Borrar archivo
+      fs.unlink(req.file.path, (error) => {
+        if (error) {
+          return res.status(500).json({
+            mensaje: "Error al eliminar el archivo no válido",
+            error
+          });
+        }
+        return res.status(400).json({
+          mensaje: "extencion no valida"
+        });
+      });
+    } else {
+      try {
+        // Obtener el ID del artículo desde los parámetros de la solicitud
+        let id = req.params.id;
+  
+        // Buscar y actualizar el artículo
+        let foto = await publicacion.findByIdAndUpdate({user:req.user._id, _id:idpublicacion }, { archivo: req.file.filename }, { new: true });
+  
+        if (!foto) {
+          // Borrar el archivo si el artículo no se encuentra
+          fs.unlink(req.file.path, (error) => {
+            return res.status(404).json({
+              mensaje: "No se encontró el artículo"
+            });
+          });
+        } else {
+          return res.status(200).json({
+            status: "success",
+            mensaje: "Imagen subida y artículo actualizado correctamente",
+  
+  
+          });
+        }
+      } catch (error) {
+        // Borrar el archivo en caso de error
+        fs.unlink(req.file.path, (err) => {
+          return res.status(500).json({
+            mensaje: "Error al modificar el artículo",
+            error
+          });
+        });
+      }
+    }
+  };
+
+//listar las publicaciones de los usuarios que sigo
+const listarPublicacionesSeguidos = async (req, res) => {
+    try {
+        // Obtener el ID del usuario autenticado
+        const userId = req.user._id;
+
+        // Buscar los usuarios seguidos por el usuario autenticado
+        const usuariosSeguidos = await Seguidor.find({ seguidor: userId }).select('usuarioSeguido');
+
+        // Obtener los IDs de los usuarios seguidos
+        const usuariosSeguidosIds = usuariosSeguidos.map(usuario => usuario.usuarioSeguido);
+
+        // Buscar las publicaciones de los usuarios seguidos
+        const publicaciones = await publicacion.find({ user: { $in: usuariosSeguidosIds } });
+
+        return res.status(200).json({
+            status: "success",
+            message: "Publicaciones de usuarios seguidos encontradas",
+            publicaciones,
+        });
+    } catch (error) {
+        console.error(error); // Log del error para depuración
+        return res.status(500).json({
+            status: "error",
+            message: "Error del servidor",
+            error: error.message,
+        });
+    }
+};
+
+
+
 
 //subir imagenes 
 
@@ -64,5 +283,9 @@ const savePublicacion =async(req,res)=>{
 //buscar imagenes 
 //exportar modulo   
 module.exports = {
-    savePublicacion
+    savePublicacion,
+    buscarPublicacion,
+    EliminarPublicacion,
+    listarPublicacionesUsuario,
+    subirImagen
 }
--- api-res-redsocial/rutas/publicacion.js
+++ api-res-redsocial/rutas/publicacion.js
@@ -3,8 +3,25 @@ const express = require('express');
 const router = express.Router();
 const daopublicacion = require('../controladores/DAOpublicacion');
 const check = require('../middleware/auth.js');
+const multer = require("multer");
+
+
+
+//configurar multer
+const almacenamiento = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./imagenes/fotoperfil/publicaciones/");
+    },
+    filename: (req, file, cb) => {
+        cb(null, "publicacion" + Date.now() + file.originalname);
+    }
+});
+const subida = multer({ storage: almacenamiento });
 //definir rutas
 router.post("/guardarpubli",check.auth, daopublicacion.savePublicacion);
-
+router.get("/buscarpubli/:id",check.auth, daopublicacion.buscarPublicacion);
+router.delete("/eliminarpubli/:id",check.auth, daopublicacion.EliminarPublicacion);
+router.get("/listarPublicion/:id/:page?",check.auth, daopublicacion.listarPublicacionesUsuario);
+router.post("/subir/:id",[check.auth,subida.single("file0")], daopublicacion.subirImagen);
 //exportar modulo
 module.exports = router;
